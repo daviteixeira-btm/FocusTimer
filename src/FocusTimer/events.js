435a3119f67a34ce2435d180dc8fcc43cc91819a
Davi Teixeira <daviteixeira-btm@gmail.com>
2024-03-17 23:17:05 -0300

    [Davi] Connecting the first actions

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,11 +1,14 @@
+import * as actions from "./actions";
 import { controls } from "./elements";
 
 export function registerControls(){
     controls.addEventListener('click', (event) => {
         const action = event.target.dataset.action;
 
-        if(action === undefined){
+        if(typeof actions[action] != "function"){
             return;
         }
+
+        actions[action]()
     });
 }
