7a572f034e9cc6b237b6aaeb6cf8fc59f452b598
Davi Teixeira <daviteixeira-btm@gmail.com>
2024-03-21 02:30:17 -0300

    [Davi] Updating the counter display

--- src/FocusTimer/index.js
+++ src/FocusTimer/index.js
@@ -1,9 +1,12 @@
 import state from "./state.js";
+import * as timer from "./timer.js";
 import * as events from "./events.js";
 
 export function start(minutes, seconds){
     state.minutes = minutes;
     state.seconds = seconds;
 
-    events.registerControls()
+    timer.updateDisplay();
+
+    events.registerControls();
 }
