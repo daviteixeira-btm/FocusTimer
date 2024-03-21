63637e2e1d340dbf3413936b4fb1a9e7197ddd41
Davi Teixeira <daviteixeira-btm@gmail.com>
2024-03-21 03:28:51 -0300

    [Davi] Finalizing the choice of timer minutes

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,6 +1,8 @@
+import state from "./state.js";
 import * as el from "./elements.js";
 import * as actions from "./actions.js";
 import { controls } from "./elements.js";
+import { updateDisplay } from "./timer.js";
 
 export function registerControls(){
     controls.addEventListener('click', (event) => {
@@ -20,4 +22,15 @@ export function setMinutes(){
     });
 
     el.minutes.onkeypress = (event) => /\d/.test(event.key);
+
+    el.minutes.addEventListener('blur', (event) => {
+        let time = event.currentTarget.textContent;
+        time = time > 60 ? 60 : time;
+
+        state.minutes = time;
+        state.seconds = 0;
+
+        updateDisplay();
+        el.minutes.removeAttribute('contenteditable');
+    });
 };
